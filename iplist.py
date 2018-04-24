d1ec8fa30aea0653719836d7fac3f451fadee044
ofir <iluzofir@gmail.com>
2018-04-24 14:24:01 -0700

    Init commit on GUI

---
+++ iplist.py
@@ -0,0 +1,28 @@
+from kivy.uix.listview import ListView, ListItemButton
+from kivy.adapters.listadapter import ListAdapter
+from kivy.logger import Logger
+
+
+class IPList(ListView):
+    def __init__(self, **kwargs):
+        self.args_converter = lambda row_index, ip: {'text': ip,
+                                                 'size_hint_y': None,
+                                                 'size_hint_x': 1.0,
+                                                 'height': 60}
+        self.list_adapter = ListAdapter(data=['123.123.123.123'],
+                                        selection_mode='single',
+                                        cls=ListItemButton,
+                                        args_converter=self.args_converter,
+                                        allow_empty_selection=False)
+        kwargs['adapter'] = self.list_adapter
+        super(ListView, self).__init__(**kwargs)
+
+    def add_ip(self, ip):
+        Logger.fatal("Adding IP = " + ip)
+        self.list_adapter.data.append(ip)
+        self.populate()
+
+    def remove_ip(self, ip):
+        if self.list_adapter.data.index(ip) != -1:
+            Logger.fatal("Removing IP = " + ip)
+            self.list_adapter.data.remove(ip)
---
+++ main.py
@@ -0,0 +1,49 @@
+__version__ = 1.0
+
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.popup import Popup
+from kivy.uix.gridlayout import GridLayout
+from iplist import IPList
+from kivy.logger import Logger
+
+
+class ControlLayout(GridLayout):
+    def on_scan_cb(self, instance):
+        Logger.fatal("Inside Callback")
+
+    def __init__(self, **kwargs):
+        Logger.fatal("THIS IS THE BEGINNING")
+        self.ip_list = IPList(size_hint_y=0.8)
+        self.ip_list.add_ip("127.0.0.1")
+        self.scan_button = Button(text="Scan", size_hint_y=0.2)
+        self.scan_button.bind(state=self.on_scan_cb)
+        kwargs['cols'] = 1
+        kwargs['size_hint'] = (1.0, 1.0)
+        # kwargs['row_force_default'] = True
+        # kwargs['row_default_height'] = 40
+
+        super(ControlLayout, self).__init__(**kwargs)
+
+        self.add_widget(self.scan_button)
+        self.add_widget(self.ip_list)
+
+    def get_ip_widger(self):
+        return self.ip_list
+
+
+class MainApp(App):
+    def build(self):
+        layout = ControlLayout()
+        return layout
+
+
+if __name__ == "__main__":
+    try:
+        MainApp().run()
+    except Exception, e:
+        Logger.fatal(e.message)
+        Logger.fatal(str(e))
+        popup = Popup(title='ERROR', content=Label(text=str(e)),auto_dismiss=False)
+        popup.open()
